57fe81f78214ee1a9f35c35121c3e1f7e28a5143
Leonardo A <leonardo@hero99.com.br>
2018-11-21 17:34:08 -0200

    First Commit

---
+++ polybar/.config/polybar/openweathermap.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+KEY="ca3723ed9d38532d0e39cc23c17cf6f6"
+CITY="Curitiba,BR"
+UNITS="metric"
+SYMBOL="°"
+LANG="pt"
+
+API="https://api.openweathermap.org/data/2.5"
+
+if [ ! -z $CITY ]; then
+    if [ "$CITY" -eq "$CITY" ] 2>/dev/null; then
+        CITY_PARAM="id=$CITY"
+    else
+        CITY_PARAM="q=$CITY"
+    fi
+
+    weather=$(curl -sf "$API/weather?appid=$KEY&$CITY_PARAM&units=$UNITS&lang=$LANG")
+else
+    location=$(curl -sf https://location.services.mozilla.com/v1/geolocate?key=geoclue)
+
+    if [ ! -z "$location" ]; then
+        location_lat="$(echo "$location" | jq '.location.lat')"
+        location_lon="$(echo "$location" | jq '.location.lng')"
+
+        weather=$(curl -sf "$API/weather?appid=$KEY&lat=$location_lat&lon=$location_lon&units=$UNITS")
+    fi
+fi
+
+if [ ! -z "$weather" ]; then
+    weather_desc=$(echo "$weather" | jq -r ".weather[0].description")
+    weather_temp=$(echo "$weather" | jq ".main.temp" | cut -d "." -f 1)
+    weather_icon=$(echo "$weather" | jq -r ".weather[0].icon")
+
+    echo "$weather_desc", "$weather_temp$SYMBOL"
+fi
